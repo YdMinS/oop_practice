9071de58e3727cdd8d294eeec21a83d7e8452619
ydmins <dr.ydmins@gmail.com>
2023-05-01 20:19:33 +0900

    Calculator, CalculatorTest UPDATE
    CalculatorTest : @Test에서 @ParameterizedTest로 변경
     @MethodSource를 이용해 대입할 인자를 만들어주는 메서드를 매핑
     모든 테스틑 calculateTest 하나로 통합
    Calculator :
     if와 if-else문으로 사칙연산 메서드 calculate 작성

--- src/test/java/com/ydmins/CalculatorTest.java
+++ src/test/java/com/ydmins/CalculatorTest.java
@@ -2,7 +2,13 @@ package com.ydmins;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * 요구사항
@@ -16,18 +22,21 @@ public class CalculatorTest {
     // 1 + 2 ---> Calculator
     // Calculator ---> 3
     @DisplayName("덧셈 연산을 정상적으로 수행한다.")
-    @Test
-    void additionTest() {
-        int result = Calculator.calculate(1, "+", 2);
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void calculateTest(int operand1, String operator, int operand2, int result) {
+        int calculatedResult = Calculator.calculate(operand1, operator, operand2);
 
-        Assertions.assertThat(result).isEqualTo(3);
+        Assertions.assertThat(calculatedResult).isEqualTo(result);
     }
 
-    @DisplayName("뺄셈 연산을 정상적으로 수행한다.")
-    @Test
-    void substractionTest() {
-        int result = Calculator.calculate(1, "-", 2);
-
-        Assertions.assertThat(result).isEqualTo(-1);
+    private static Stream<Arguments> formulaAndResult(){
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(1, "*", 2, 2),
+                arguments(3, "/", 2, 1)
+        );
     }
+
 }
