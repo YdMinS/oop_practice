3ce17f443bdafa0332184ae6352e5bb9fc63d270
ydmins <dr.ydmins@gmail.com>
2023-05-02 11:38:18 +0900

    PositiveNumber Test Generate, CalculatorTest EDIT
    PositiveNumberTest 생성
    CalculatorTest에서 0 체크하는 테스트는 필요없어져 삭제

--- src/test/java/com/ydmins/CalculatorTest.java
+++ src/test/java/com/ydmins/CalculatorTest.java
@@ -1,8 +1,8 @@
 package com.ydmins;
 
+import com.ydmins.calculate.PositiveNumber;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -26,7 +26,7 @@ public class CalculatorTest {
     @ParameterizedTest
     @MethodSource("formulaAndResult")
     void calculateTest(int operand1, String operator, int operand2, int result) {
-        int calculatedResult = Calculator.calculate(operand1, operator, operand2);
+        int calculatedResult = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
 
         Assertions.assertThat(calculatedResult).isEqualTo(result);
     }
@@ -40,10 +40,4 @@ public class CalculatorTest {
         );
     }
 
-    @DisplayName("0을 나누는 경우 IllegalArgument 예외를 발생시킨다.")
-    @Test
-    void calculateExceptionTest() {
-        Assertions.assertThatCode(()->Calculator.calculate(10, "/", 0))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
 }
